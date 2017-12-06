af66f64c13499a935608e58fcc8a96f7556b21c0
duff.brian21@gmail.com <duff.brian21@gmail.com>
2017-12-05 20:10:48 -0800

    added file structure

---
+++ assets/javascript/script.js
@@ -0,0 +1,18 @@
+// make a giphy account & get api key
+// create html structure add buttons for the buttons that are supposed to preload(already on the page)
+// create form to add new buttons
+// create variables in array
+
+// create an .ajax call 
+// console.log respose to test
+// dissect the object
+    // title
+    // ratings
+    // img (limit the number of images displayed)
+// display to user
+// on click animate gifs
+// on click stops animation
+
+
+(document).ready
+clear
